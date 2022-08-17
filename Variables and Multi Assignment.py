0c94150541a4b9e1de0d46d32f898716c8be453e
Muhammad Bilal Saeed <bilalsaeed18.bs@gmail.com>
2022-08-17 04:36:52 -0700

    variables and multiple assignments

---
+++ Variables and Multi Assignment.py
@@ -0,0 +1,13 @@
+# For the Variable we are using python console to assign variable
+
+#In Python, use the = operator to assign values to variables.
+
+Age = 25
+
+
+#You can assign values to multiple variables on one line.
+
+#Assign multiple values to multiple variables
+Bilal, Ali, Saad = 20,22,16
+#Assign the same value to multiple variables
+Bilal, Ali, Saad = 20
--- readme.md
+++ readme.md
@@ -3,3 +3,6 @@ Python Basics for beginners
 Download Python from official website 
 https://www.python.org/ 
 with the latest version 3.10
+
+download pycharm IDE
+https://www.jetbrains.com/pycharm/download/#section=windows
