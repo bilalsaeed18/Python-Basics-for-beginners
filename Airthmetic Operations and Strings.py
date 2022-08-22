252619f202149074d496f81ca99b28c249fdcece
Muhammad Bilal Saeed <bilalsaeed18.bs@gmail.com>
2022-08-22 01:20:02 -0700

    Add Arithmetic operations and strings

---
+++ Airthmetic Operations and Strings.py
@@ -0,0 +1,13 @@
+# For the Arithmetic operations we are using python console to assign variable
+
+#Arithmetic operators are used to perform mathematical operations like addition, subtraction, multiplication and division.
+
+#There are 7 arithmetic operators in Python :
+
+#Addition
+#Subtraction
+#Multiplication
+#Division
+#Modulus
+#Exponentiation
+#Floor division
